6a994ef47086233df44e406b615658d8d5daf90b
Nick <99460436+nicolaslee2000@users.noreply.github.com>
2023-04-09 22:06:51 +0200

    changed index and app.js to tsx

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+import "./style.css";
+// import Router from "./routes/Router";
+// import { AuthContextProvider } from "./context/AuthContext";
+
+function App() {
+  return (
+    // <AuthContextProvider>
+    <div className="App">
+      {/* <Router /> */}
+      hi
+    </div>
+    // </AuthContextProvider>
+  );
+}
+
+export default App;

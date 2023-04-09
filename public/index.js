43e0a1699fe496c9a029a2e2c52242f88d4096a2
Nick <99460436+nicolaslee2000@users.noreply.github.com>
2023-04-09 21:57:35 +0200

    fixed peer dependency error

---
+++ public/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+var root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(React.createElement(React.StrictMode, null,
+    React.createElement(App, null)), document.getElementById("root"));
+reportWebVitals();
+//# sourceMappingURL=index.js.map

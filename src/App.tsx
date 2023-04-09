25ca2c8e2545fa8cebb57e2898d48eb6336b7973
Nick <99460436+nicolaslee2000@users.noreply.github.com>
2023-04-09 22:16:29 +0200

    changed app.css according to ksv website

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
 import React from "react";
 import "./App.css";
 import "./style.css";
-// import Router from "./routes/Router";
-// import { AuthContextProvider } from "./context/AuthContext";
+import Router from "./routes/Router";
+import { AuthContextProvider } from "./context/AuthContext";
 
 function App() {
   return (
-    // <AuthContextProvider>
-    <div className="App">
-      {/* <Router /> */}
-      hi
-    </div>
-    // </AuthContextProvider>
+    <AuthContextProvider>
+      <div className="App">
+        <Router />
+      </div>
+    </AuthContextProvider>
   );
 }
 

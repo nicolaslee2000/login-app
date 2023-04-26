61dc3a443c69a91079de4e50a5d40c529691fbf2
Nick <99460436+nicolaslee2000@users.noreply.github.com>
2023-04-26 17:18:39 +0200

    fgg

--- src/firebase-config.ts
+++ src/firebase-config.ts
@@ -1,17 +1,17 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, updateProfile } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getStorage} from "firebase/storage";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDkZcaqSkfStmp6XppcBYfPuiHaIm9LlIw", //not publish in github
-    authDomain: "loginapp-deb69.firebaseapp.com", //not publish in github
-    projectId: "loginapp-deb69",
-    storageBucket: "loginapp-deb69.appspot.com",
-    messagingSenderId: "702229184126",
-    appId: "1:702229184126:web:f497a5beefbc698934622f",
-    measurementId: "G-K19EL9259W"
-  };
+  apiKey: "AIzaSyDkZcaqSkfStmp6XppcBYfPuiHaIm9LlIw", //not publish in github
+  authDomain: "loginapp-deb69.firebaseapp.com", //not publish in github
+  projectId: "loginapp-deb69",
+  storageBucket: "loginapp-deb69.appspot.com",
+  messagingSenderId: "702229184126",
+  appId: "1:702229184126:web:f497a5beefbc698934622f",
+  measurementId: "G-K19EL9259W",
+};
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
